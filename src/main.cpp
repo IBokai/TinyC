d74247af5dd34318d19f6df8e08f87874d59d7c4
Ivan <bokai.box@gmail.com>
2025-04-23 23:52:28 +0300

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,11 @@
 #include "AST/ASTNode.h"
 
 int main() {
-    std::unique_ptr<Expression> left = std::make_unique<NumberExpression>(NumberExpression(3));
-    std::string name = "name";
-    std::unique_ptr<Statement> assign = std::make_unique<AssignmentStatement>(AssignmentStatement(std::move(name), std::move(left)));
-//     Lexer lexer("../samples/basic.txt");
-//     auto v = lexer.Tokenize();
-//     std::cout << v.size() << '\n';
-//     for (int i = 0; i < v.size(); i++) {
-//         std::cout << v[i].text << '\n';
-//     }
-//     return 0;
+    Lexer lexer("../samples/basic.txt");
+    auto v = lexer.Tokenize();
+    std::cout << v.size() << '\n';
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i].text << '\n';
+    }
+    return 0;
 }
