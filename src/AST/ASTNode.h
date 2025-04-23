edf5644d43846fef598b07132640f9787868a173
Ivan <bokai.box@gmail.com>
2025-04-23 23:14:08 +0300

    feat: add constructors for AST classes

--- src/AST/ASTNode.h
+++ src/AST/ASTNode.h
@@ -10,30 +10,63 @@ class Expression : public ASTNode {};
 class Statement : public ASTNode {};
 
 class NumberExpression : public Expression {
-    int value;
+public:
+    NumberExpression(int value) : value_(value) {}
+
+private:
+    int value_;
 };
 
 class VariableExpression : public Expression {
-    std::string name;
+public:
+    VariableExpression(std::string name) : name_(std::move(name)) {}
+
+private:
+    std::string name_;
 };
 
 class BinaryExpression : public Expression {
-    std::unique_ptr<Expression> left;
-    std::string op;  // operation, maybe replace it with enum class or something
-    std::unique_ptr<Expression> right;
+public:
+    BinaryExpression(std::unique_ptr<Expression> left, std::string op,
+                     std::unique_ptr<Expression> right)
+        : left_(std::move(left)), op_(std::move(op)), right_(std::move(right)) {}
+
+private:
+    std::unique_ptr<Expression> left_;
+    std::string op_;  // operation, maybe replace it with enum class or something
+    std::unique_ptr<Expression> right_;
 };
 
 class AssignmentStatement : public Statement {
-    std::string name;
-    std::unique_ptr<Expression> expression;
+public:
+    AssignmentStatement(std::string name, std::unique_ptr<Expression> expression)
+        : name_(name), expression_(std::move(expression)) {}
+
+private:
+    std::string name_;
+    std::unique_ptr<Expression> expression_;
 };
 
 class IfStatement : public Statement {
-    std::unique_ptr<Expression> condition;
-    std::vector<std::unique_ptr<Statement>> body;
+public:
+    IfStatement(std::unique_ptr<Expression> condition, std::unique_ptr<Statement> bodyStatement)
+        : condition_(std::move(condition)) {
+        body_.push_back(std::move(bodyStatement));
+    }
+
+private:
+    std::unique_ptr<Expression> condition_;
+    std::vector<std::unique_ptr<Statement>> body_;
 };
 
 class WhileStatement : public Statement {
-    std::unique_ptr<Expression> condition;
-    std::vector<std::unique_ptr<Statement>> body;
+public:
+    WhileStatement(std::unique_ptr<Expression> condition, std::unique_ptr<Statement> bodyStatement)
+        : condition_(std::move(condition)) {
+        body_.push_back(std::move(bodyStatement));
+    }
+
+private:
+    std::unique_ptr<Expression> condition_;
+    std::vector<std::unique_ptr<Statement>> body_;
 };
